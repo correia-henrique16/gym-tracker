005afc4d978e11a82a437d0346050875b54e4a81
correia-henrique16 <correiahenrique8@gmail.com>
2026-04-14 18:04:11 +0100

    Correção de bug na barra de cima do iphone

--- src/styles/components/TituloListar.jsx
+++ src/styles/components/TituloListar.jsx
@@ -1,7 +1,7 @@
 const TituloListar = ({children, className=""}) => {
     return(
         <h1
-            className="text-3xl font-bold text-texto sticky top-0 bg-fundo w-full text-center box-border py-4 px-15 z-3"
+            className="text-3xl font-bold text-texto sticky top-0 bg-fundo w-full text-center box-border py-4 px-15 pt-14 z-3"
         >
             {children}
         </h1>
