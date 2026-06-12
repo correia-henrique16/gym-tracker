e36cd2b7bfa5989e24d25bc462a3f6f2587577c8
correia-henrique16 <correiahenrique8@gmail.com>
2026-06-12 11:44:15 +0100

    Melhorar responsividade nav-bar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -22,8 +22,8 @@ const Home = () => {
 
             <div className="w-11/12 top-0 flex justify-between items-center box-border absolute py-5 z-100 bg-fundo/95">
                 <div className="flex justify-baseline items-center gap-2 cursor-default w-1/3">
-                    <img src={imgInicial} alt="Barbell" className="max-h-10"/>
-                    <h1 className="text-verde text-xl">Gym Tracker</h1>
+                    <img src={imgInicial} alt="Barbell" className="md:max-h-10 max-h-8"/>
+                    <h1 className="text-verde text-lg md:text-xl">Gym Tracker</h1>
                 </div>
                 
                 <div className="w-1/3 flex justify-center items-center">
@@ -33,12 +33,15 @@ const Home = () => {
                 </div>
                 
                 
-                <Link to={`/profile`} className="flex items-center justify-center gap-4 w-1/3">
-                    <p className="align-middle text-verde text-2xl">{userName}</p>
-                    <div className="w-16 h-16 rounded-full bg-verde flex items-center justify-center">
-                        <p className="text-white  text-4xl">{userName?.charAt(0).toUpperCase()}</p>
-                    </div>
-                </Link>
+                <div className="flex items-center justify-end w-1/3">
+                    <Link to={`/profile`} className="flex items-center gap-2 md:gap-4 justify-end">
+                        <p className="hidden md:block! align-middle text-verde text-2xl">{userName}</p>
+                        <div className="w-16 h-16 rounded-full bg-verde flex items-center justify-center">
+                            <p className="text-white  text-4xl">{userName?.charAt(0).toUpperCase()}</p>
+                        </div>
+                    </Link>
+                </div>
+                
             </div>
             
 
--- src/styles/components/LinkBtn.jsx
+++ src/styles/components/LinkBtn.jsx
@@ -4,8 +4,8 @@ const LinkBtn = ({children, className = "", ...props}) => {
     return(
         <Link
             {...props}
-            className="p-2 text-lg rounded-2xl w-30 cursor-pointer bg-verde/20 text-texto border-verde
-            border-1 flex justify-center items-center hover:bg-verde-cinza-hover"
+            className="p-2 text-base md:text-lg rounded-2xl w-24 md:w-30 cursor-pointer bg-verde/20 text-texto border-verde
+            border flex justify-center items-center hover:bg-verde-cinza-hover"
         >
             {children}
         </Link>
